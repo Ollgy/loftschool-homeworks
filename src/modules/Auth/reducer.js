9e95babb5fafe15edad3b4e3bce9ef8e78364bb1
Olga Baranova <olga.baranova@uchi.ru>
2019-05-16 17:11:20 +0300

    clean

--- src/modules/Auth/reducer.js
+++ src/modules/Auth/reducer.js
@@ -7,18 +7,6 @@ import { addApiKey } from './actions';
 // Имеет смысл определить селекторы
 // getIsAuthorized, getApiKey
 
-// const auth = (state = {}, action) => {
-//   switch (action.type) {    
-//     case "ADD_API_KEY":
-//       console.log(action.payload);
-//       return { ...state, 
-//         key: action.payload, 
-//         isAuthorized: action.payload === 'https://github.com/ on КП at 04-фев-2018 00:34' };
-//     default:
-//       return state;
-//   }
-// }
-
 const apiKey = handleActions(
   {
     [addApiKey]: (state, action) => action.payload
